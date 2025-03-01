3354286434150495446e1fc8bf5d1cec1796ed2f
armoney1 <158165288+SeKreT1179@users.noreply.github.com>
2025-03-01 15:32:50 +0300

    add Student

--- src/main/java/org/example/edu/controller/HellowController.java
+++ src/main/java/org/example/edu/controller/HellowController.java
@@ -1,5 +1,6 @@
 package org.example.edu.controller;
 
+import org.example.edu.model.Student;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,7 +9,16 @@ public class HellowController {
 
     @GetMapping("/Hello")
     public String Hello(String name) {
-        return "Hello" + name;
+        return "Hello " + name;
+    }
+
+    @GetMapping("/student")
+    public Student getStudent() {
+        Student st = new Student(1,
+                "valera",
+                "valera@ya.ru",
+                19);
+        return st;
     }
 
 }
